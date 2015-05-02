f7b0c2140f8c4d9396a5b499f0a7906ea6fa0e88
xunmengwuya <tl.0623@163.com>
2015-05-03 01:48:49 +0800

    ......

--- app/src/main/java/com/fmc/edu/MainActivity.java
+++ app/src/main/java/com/fmc/edu/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); test
+        setContentView(R.layout.activity_main);
     }
 }
