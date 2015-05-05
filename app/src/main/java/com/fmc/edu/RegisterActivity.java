1f696da62b17be43c126db85c11ef7975329939d
tanglan <553852807@qq.com>
2015-05-05 14:17:02 +0800

    注册页面完善

--- app/src/main/java/com/fmc/edu/RegisterActivity.java
+++ app/src/main/java/com/fmc/edu/RegisterActivity.java
@@ -2,14 +2,12 @@ package com.fmc.edu;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 
-import com.fmc.edu.enums.WatcherTypeEnum;
+import com.fmc.edu.common.MyTextWatcher;
 import com.fmc.edu.customcontrol.ValidateButtonControl;
 import com.fmc.edu.utils.StringUtils;
 import com.fmc.edu.utils.ValidationUtils;
@@ -44,83 +42,61 @@ public class RegisterActivity extends Activity {
 
     private void initViewEvents() {
         btnNextStep.setOnClickListener(btnNextStepOnClickListener);
-        MyTextWatcher validatePhoneTextWatcher = new MyTextWatcher(editCellphone, WatcherTypeEnum.ValidateCell);
+
+        MyTextWatcher validatePhoneTextWatcher = new MyTextWatcher();
         editCellphone.addTextChangedListener(validatePhoneTextWatcher);
+        validatePhoneTextWatcher.setOnTextChangedListener(validateCellphoneTextChangeListener);
+
+        MyTextWatcher validateInputTextWatcher = new MyTextWatcher();
+        editAuthCode.addTextChangedListener(validateInputTextWatcher);
+        validateInputTextWatcher.setOnTextChangedListener(validateInputTextChangeListener);
+
+        editPassword.addTextChangedListener(validateInputTextWatcher);
+        validateInputTextWatcher.setOnTextChangedListener(validateInputTextChangeListener);
+
+        ckReadAgreement.setOnClickListener(ckReadAgreementOnClickListener);
     }
 
     private View.OnClickListener btnNextStepOnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-
+            //TODO ÏÂÒ»²½
         }
     };
 
-    class MyTextWatcher implements TextWatcher {
-
-        EditText editText;
-        WatcherTypeEnum checkCode;
-
-        public MyTextWatcher(EditText editText, WatcherTypeEnum checkCode) {
-            this.editText = editText;
-            this.checkCode = checkCode;
-        }
-
-
+    private View.OnClickListener ckReadAgreementOnClickListener = new View.OnClickListener() {
         @Override
-        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
+        public void onClick(View v) {
+            CheckValidateInput();
         }
+    };
 
+    private MyTextWatcher.OnTextChangedListener validateCellphoneTextChangeListener = new MyTextWatcher.OnTextChangedListener() {
         @Override
-        public void onTextChanged(CharSequence s, int start, int before, int count) {
-
+        public void onTextChanged() {
+            String cellphoneNum = editCellphone.getText().toString();
+            boolean isEffectPhone = !StringUtils.isEmptyOrNull(cellphoneNum) && ValidationUtils.isMobilePhone(cellphoneNum);
+            validateBtnGetAuthCode.setEnabled(isEffectPhone);
+            CheckValidateInput();
         }
+    };
 
+
+    private MyTextWatcher.OnTextChangedListener validateInputTextChangeListener = new MyTextWatcher.OnTextChangedListener() {
         @Override
-        public void afterTextChanged(Editable s) {
-            isShowNextStep();
+        public void onTextChanged() {
+            CheckValidateInput();
         }
+    };
 
-        private void isShowNextStep() {
-            switch (checkCode) {
-                case ValidateCell:
-                    SetValidatePhone();
-//                case 1:
-//                    SetInputInfo();
-            }
+    private void CheckValidateInput() {
+        boolean isValidatePhone = !StringUtils.isEmptyOrNull(editCellphone.getText().toString()) && ValidationUtils.isMobilePhone(editCellphone.getText().toString());
+        boolean isValidateCode = !StringUtils.isEmptyOrNull(editAuthCode.getText());
+        boolean isValidatePassword = editPassword.length() >= 6 && editPassword.length() <= 16;
+        if (isValidatePhone && isValidateCode && ckReadAgreement.isChecked() && isValidatePassword) {
+            btnNextStep.setEnabled(true);
+            return;
         }
-
-        private void SetValidatePhone() {
-            String text = editText.getText().toString();
-            if (editText.getId() == R.id.register_edit_cellphone) {
-                boolean isEffectPhone = !StringUtils.isEmptyOrNull(text) && ValidationUtils.isMobilePhone(text);
-                validateBtnGetAuthCode.setEnabled(isEffectPhone);
-            }
-
-            boolean isValidatePhone = !StringUtils.isEmptyOrNull(editCellphone.getText()) && ValidationUtils.isMobilePhone(editCellphone.getText().toString());
-            boolean isValidateCode = !StringUtils.isEmptyOrNull(editAuthCode.getText());
-            boolean isValidatePassword = editPassword.length() >= 6 && editPassword.length() <= 16;
-            if (isValidatePhone && isValidateCode && ckReadAgreement.isChecked() && isValidatePassword) {
-                btnNextStep.setEnabled(true);
-                return;
-            }
-            btnNextStep.setEnabled(false);
-        }
-
-//        private void SetInputInfo() {
-//            String fullname = et_register_fullname.getText().toString();
-//            String drivingSchool = et_register_driving_school.getText().toString();
-//            String carNum = et_register_car_num.getText().toString();
-//            String cardNum = et_register_card_num.getText().toString();
-//            String password = et_register_password.getText().toString();
-//
-//            if (StringUtils.isNotBlank(fullname) && StringUtils.isNotBlank(drivingSchool)
-//                    && StringUtils.isNotBlank(carNum) && StringUtils.isNotBlank(cardNum) && StringUtils.isNotBlank(password)) {
-//                btn_to_validate_identity.setEnabled(true);
-//                return;
-//            }
-//            btn_to_validate_identity.setEnabled(false);
-//        }
+        btnNextStep.setEnabled(false);
     }
-
 }
--- app/src/main/java/com/fmc/edu/common/MyTextWatcher.java
+++ app/src/main/java/com/fmc/edu/common/MyTextWatcher.java
@@ -1,7 +1,89 @@
 package com.fmc.edu.common;
 
+import android.text.Editable;
+import android.text.TextWatcher;
+
 /**
  * Created by Candy on 2015-05-05.
  */
-public class MyTextWatcher {
+public class MyTextWatcher implements TextWatcher {
+
+//    EditText editText;
+//    WatcherTypeEnum checkCode;
+
+    public OnTextChangedListener mOnTextChangedListener;
+
+    public interface OnTextChangedListener {
+        void onTextChanged();
+    }
+
+    public MyTextWatcher() {
+//        this.editText = editText;
+//        this.checkCode = checkCode;
+    }
+
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        if (null == this.mOnTextChangedListener) {
+            return;
+        }
+        this.mOnTextChangedListener.onTextChanged();
+    }
+
+    public void setOnTextChangedListener(OnTextChangedListener mOnTextChangedListener) {
+        this.mOnTextChangedListener = mOnTextChangedListener;
+    }
+//
+//    private void isShowNextStep() {
+//        switch (checkCode) {
+//            case ValidateCell:
+//                SetValidatePhone();
+////                case 1:
+////                    SetInputInfo();
+//        }
+//    }
+//
+//    private void SetValidatePhone() {
+//        String text = editText.getText().toString();
+//        if (editText.getId() == R.id.register_edit_cellphone) {
+//            boolean isEffectPhone = !StringUtils.isEmptyOrNull(text) && ValidationUtils.isMobilePhone(text);
+//            validateBtnGetAuthCode.setEnabled(isEffectPhone);
+//        }
+//
+//        boolean isValidatePhone = !StringUtils.isEmptyOrNull(editCellphone.getText()) && ValidationUtils.isMobilePhone(editCellphone.getText().toString());
+//        boolean isValidateCode = !StringUtils.isEmptyOrNull(editAuthCode.getText());
+//        boolean isValidatePassword = editPassword.length() >= 6 && editPassword.length() <= 16;
+//        if (isValidatePhone && isValidateCode && ckReadAgreement.isChecked() && isValidatePassword) {
+//            btnNextStep.setEnabled(true);
+//            return;
+//        }
+//        btnNextStep.setEnabled(false);
+//    }
+
+//        private void SetInputInfo() {
+//            String fullname = et_register_fullname.getText().toString();
+//            String drivingSchool = et_register_driving_school.getText().toString();
+//            String carNum = et_register_car_num.getText().toString();
+//            String cardNum = et_register_card_num.getText().toString();
+//            String password = et_register_password.getText().toString();
+//
+//            if (StringUtils.isNotBlank(fullname) && StringUtils.isNotBlank(drivingSchool)
+//                    && StringUtils.isNotBlank(carNum) && StringUtils.isNotBlank(cardNum) && StringUtils.isNotBlank(password)) {
+//                btn_to_validate_identity.setEnabled(true);
+//                return;
+//            }
+//            btn_to_validate_identity.setEnabled(false);
+//        }
 }
+
