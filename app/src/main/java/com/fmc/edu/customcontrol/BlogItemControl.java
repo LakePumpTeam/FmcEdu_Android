352b7eef413b34e4ef7c29d828f3eb030cf14bf4
tanglan <553852807@qq.com>
2015-05-07 16:16:30 +0800

    登录布局

---
+++ app/src/main/java/com/fmc/edu/customcontrol/BlogItemControl.java
@@ -0,0 +1,14 @@
+package com.fmc.edu.customcontrol;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by Candy on 2015-05-07.
+ */
+public class BlogItemControl extends LinearLayout {
+    public BlogItemControl(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
