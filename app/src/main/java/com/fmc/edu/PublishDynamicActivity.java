1a67d0d58f3f6637af1659e4ae80eed0ea3cc6e9
tanglan <553852807@qq.com>
2015-05-19 19:52:25 +0800

    发布动态

--- app/src/main/java/com/fmc/edu/PublishDynamicActivity.java
+++ app/src/main/java/com/fmc/edu/PublishDynamicActivity.java
@@ -2,8 +2,6 @@ package com.fmc.edu;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.fmc.edu.common.CrashHandler;
 
@@ -18,25 +16,4 @@ public class PublishDynamicActivity extends Activity {
         setContentView(R.layout.activity_publish_dynamic);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_publish_dynamic, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
