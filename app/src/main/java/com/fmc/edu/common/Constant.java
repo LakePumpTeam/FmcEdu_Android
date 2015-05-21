c5c0e4ad602a7dee0a1a6a7320b3185edb439851
tanglan <553852807@qq.com>
2015-05-21 19:15:45 +0800

    上传图片

---
+++ app/src/main/java/com/fmc/edu/common/Constant.java
@@ -0,0 +1,7 @@
+package com.fmc.edu.common;
+
+/**
+ * Created by Candy on 2015-05-21.
+ */
+public class Constant {
+}
---
+++ app/src/main/java/com/fmc/edu/utils/RequestCodeUtils.java
@@ -0,0 +1,7 @@
+package com.fmc.edu.utils;
+
+/**
+ * Created by Candy on 2015-05-21.
+ */
+public class RequestCodeUtils {
+}
